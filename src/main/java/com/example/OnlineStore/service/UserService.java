497ad4b63ea66d08661ce9a0f7a6778fc40f8618
Aleksandr <dark.hunter@mail.ru>
2023-11-10 18:52:12 +0500

    add test, cut security

--- src/main/java/com/example/OnlineStore/service/UserService.java
+++ src/main/java/com/example/OnlineStore/service/UserService.java
@@ -1,49 +1,49 @@
-package com.example.OnlineStore.service;
-
-import com.example.OnlineStore.model.Role;
-import com.example.OnlineStore.model.User;
-import com.example.OnlineStore.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@Component
-@AllArgsConstructor
-public class UserService implements UserDetailsService {
-
-    private final UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User myUser = userRepository.findByUsername(username);
-        return new org.springframework.security.core.userdetails.User(myUser.getUsername(),
-            myUser.getPassword(), mapRolesToAthorities(myUser.getRoles()));
-    }
-
-    private List<? extends GrantedAuthority> mapRolesToAthorities(Set<Role> roles) {
-
-        return roles.stream()
-                .map(r -> new SimpleGrantedAuthority("ROLE_" + r.name())).collect(Collectors.toList());
-    }
-
-    public void addUser(User user) throws Exception {
-        User userFromDb = userRepository.findByUsername(user.getUsername());
-        if (userFromDb != null) {
-            throw new Exception("user exist");
-        }
-
-        user.setRoles(Collections.singleton(Role.USER));
-        user.setActive(true);
-        userRepository.save(user);
-    }
-}
+//package com.example.OnlineStore.service;
+//
+//import com.example.OnlineStore.model.Role;
+//import com.example.OnlineStore.model.User;
+//import com.example.OnlineStore.repository.UserRepository;
+//import lombok.AllArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.stream.Collectors;
+//
+//@Component
+//@AllArgsConstructor
+//public class UserService implements UserDetailsService {
+//
+//    private final UserRepository userRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User myUser = userRepository.findByUsername(username);
+//        return new org.springframework.security.core.userdetails.User(myUser.getUsername(),
+//            myUser.getPassword(), mapRolesToAthorities(myUser.getRoles()));
+//    }
+//
+//    private List<? extends GrantedAuthority> mapRolesToAthorities(Set<Role> roles) {
+//
+//        return roles.stream()
+//                .map(r -> new SimpleGrantedAuthority("ROLE_" + r.name())).collect(Collectors.toList());
+//    }
+//
+//    public void addUser(User user) throws Exception {
+//        User userFromDb = userRepository.findByUsername(user.getUsername());
+//        if (userFromDb != null) {
+//            throw new Exception("user exist");
+//        }
+//
+//        user.setRoles(Collections.singleton(Role.USER));
+//        user.setActive(true);
+//        userRepository.save(user);
+//    }
+//}
